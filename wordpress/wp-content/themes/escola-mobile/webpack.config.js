2b9a1e223ea3fd5be065efadbaa550f95d9fcb92
Igor Prates <igor.prates@escolamobile.com.br>
2023-07-24 17:03:52 -0300

    fix: ajuste nome do arquivo output webpack

--- wordpress/wp-content/themes/escola-mobile/webpack.config.js
+++ wordpress/wp-content/themes/escola-mobile/webpack.config.js
@@ -61,7 +61,7 @@ const config = {
     ],
   },
   output: {
-    filename: `[name].js`,
+    filename: "[name].min.js",
     path: path.resolve(__dirname, "dist"),
   },
   mode: process.env.NODE_ENV,
@@ -132,7 +132,7 @@ const config = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: `[name].css`,
+      filename: "[name].css",
     }),
     new CopyWebpackPlugin([
       {
